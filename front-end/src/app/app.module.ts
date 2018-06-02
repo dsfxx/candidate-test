f8e3380084705c631e9f8f5b56762273c8579ab7
Justin Byblyk <jbyblyk@gmail.com>
2018-06-02 09:39:30 -0400

    basic routing configured
    added template for client routing

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,10 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ClientCreateComponent } from './components/client-create/client-create.component';
 import { ClientIndexComponent } from './components/client-index/client-index.component';
 import { ClientEditComponent } from './components/client-edit/client-edit.component';
+
+const routes: Routes = [
+  {
+    path: 'client-create',
+    component: ClientCreateComponent
+  },
+  {
+    path: 'client-edit/:id',
+    component: ClientEditComponent
+  },
+  {
+    path: 'client-index',
+    component: ClientIndexComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +30,8 @@ import { ClientEditComponent } from './components/client-edit/client-edit.compon
     ClientEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
